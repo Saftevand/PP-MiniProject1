1399f51a85e91b5647a77ec34189d85004e2ab4b
Unknown <m010795@hotmail.com>
2018-09-25 15:42:21 +0200

    Better shiet

--- MiniProject1.rkt
+++ MiniProject1.rkt
@@ -3,21 +3,31 @@
 ;Author Mathias Lundhede Hansen Mlha15@student.aau.dk
 
 
-(define pitch pitchvalue)
+(define (pitch pitchvalue)
+  (if(and (>= pitchvalue 0) (<= pitchvalue 127)) pitchvalue (error "Invalid pitchvalue!")))
 
-(define duration timeunits)
+;(define (duration timeunits) (timeunits))
 
-(define instrument type)
+(define (instrument type)
+  (cond((eq? type "Piano")      "Piano")
+       ((eq? type "Organ")      "Organ")
+       ((eq? type "Guitar")     "Guitar")
+       ((eq? type "Violin")     "Violin")
+       ((eq? type "Flute")      "Flute")
+       ((eq? type "Trumpet")    "Trumpet")
+       ((eq? type "Helicopter") "Helicopter")
+       ((eq? type "Telephone")  "Telephone")
+       (else (error "Invalid instrument"))))
 
-(define note pitch duration instrument )
+(define (note pitch duration instrument) (1))
 
-(define pause duration )
+(define (pause duration) (duration))
 
-(define MusicElement )
+(define (MusicElement) (1))
 
-(define SequentialMusicElement )
+(define (SequentialMusicElement) (1))
 
-(define ParallelMusicElement )
+(define (ParallelMusicElement) (1))
 
 
 
