efd5df34777039a49eb137a20fdc29125de03efd
Unknown <m010795@hotmail.com>
2018-09-25 14:25:59 +0200

    Initial file creation

---
+++ MiniProject1.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;Author Mathias Lundhede Hansen Mlha15@student.aau.dk
+
+
+(define pitch )
+
+(define duration )
+
+(define instrument )
+
+(define note pitch duration instrument )
+
+(define MusicElement )
+
+(define SequentialMusicElement )
+
+(define ParallelMusicElement )
+
+
+
