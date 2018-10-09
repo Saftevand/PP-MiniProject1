f7592b05cb8e2ece4a7ab4faa40e474c313acb78
Lundhede <m010795@hotmail.com>
2018-10-09 10:10:25 +0200

    list-builder og convert af note og pause

--- MiniProject1.rkt
+++ MiniProject1.rkt
@@ -10,7 +10,7 @@
 
 ;Predicates
 (define (pitch? pitchvalue)
-  (and (>= pitchvalue 0) (<= pitchvalue 127)))
+  (and (real? pitchvalue) (>= pitchvalue 0) (<= pitchvalue 127)))
 
 (define (duration? x)
   (and (real? x) (>= x 0)))
@@ -22,13 +22,13 @@
   (if(real? type) (and(>= type 0)(<= type 8)) #f))
 
 (define (note? x)
-  (eqv? 'Note (send 'get-type x)))
+  (and ((procedure? x) (eqv? 'Note (send 'get-type x)))))
 
 (define (pause? x)
-  (eqv? 'Pause (send 'get-type x)))
+  ((and (procedure? x) (eqv? 'Pause (send 'get-type x)))))
 
 (define (sequentialMusicElement? x)
-  (eqv? 'SequentialMusicElement (send 'get-type x)))
+  (and ((procedure? x) (eqv? 'SequentialMusicElement (send 'get-type x)))))
 
 (define (parallelMusicElement? x)
   (eqv? 'ParallelMusicElement (send 'get-type x)))
@@ -74,41 +74,62 @@
   (note-abs-time-with-duration (send 'get-start-time x) 1 1 0 (send 'get-duration x)))
 
 (define (convert-to-note-abs-time-with-duration-sequential x)
-  ())
+  (if(null? x)
+     '()
+     (cond ((eqv? 'Note (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-note (car x)) (convert-to-note-abs-time-with-duration-sequential (cdr x))))
+           ((eqv? 'Pause (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-pause (car x)) (convert-to-note-abs-time-with-duration-sequential (cdr x))))
+           ((eqv? 'SequentialMusicElement (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-sequential (car x)) (convert-to-note-abs-time-with-duration-sequential (cdr x))))
+           ((eqv? 'ParallelMusicElement (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-parallel (car x)) (convert-to-note-abs-time-with-duration-sequential (cdr x))))
+           (else error "Unknown type!"))))
+           
 
 (define (convert-to-note-abs-time-with-duration-parallel x)
-  ())
-
-
-(define (list-builder lst result-lst)
-  (if(null? lst)
+  ((if(null? x)
      '()
-     (if (pair? lst)
-     (cond ((and (procedure? (car lst)) (eqv? 'Note (send 'get-type (car lst))))
-              (cons result-lst (cons (car lst) (list-builder (cdr lst) result-lst))))
+     (cond ((eqv? 'Note (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-note (car x)) (convert-to-note-abs-time-with-duration-parallel (cdr x))))
+           ((eqv? 'Pause (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-pause (car x)) (convert-to-note-abs-time-with-duration-parallel (cdr x))))
+           ((eqv? 'SequentialMusicElement (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-sequential (car x)) (convert-to-note-abs-time-with-duration-parallel (cdr x))))
+           ((eqv? 'ParallelMusicElement (send 'get-type (car x)))
+              (cons (convert-to-note-abs-time-with-duration-parallel (car x)) (convert-to-note-abs-time-with-duration-parallel (cdr x))))
+           (else error "Unknown type!")))))
+
+
+(define (list-builder x) ;result-lst)
+  (if(null? x)
+     '()
+     (if (pair? x)
+     (cond ((and (procedure? (car x)) (eqv? 'Note (send 'get-type (car x))))
+              (cons (car x) (list-builder (cdr x))))
            
-           ((and (procedure? (car lst)) (eqv? 'Pause (send 'get-type (car lst))))
-              (cons result-lst (cons (car lst) (list-builder (cdr lst) result-lst))))
+           ((and (procedure? (car x)) (eqv? 'Pause (send 'get-type (car x))))
+              (cons (car x) (list-builder (cdr x))))
            
-           ((and (procedure? (car lst)) (eqv? 'SequentialMusicElement (send 'get-type (car lst))))
-              (cons result-lst (cons (list-builder (send 'get-elements (car lst)) result-lst) (list-builder (cdr lst) result-lst))))
+           ((and (procedure? (car x)) (eqv? 'SequentialMusicElement (send 'get-type (car x))))
+              (cons (list-builder (send 'get-elements (car x))) (list-builder (cdr x))))
            
-           ((and (procedure? (car lst)) (eqv? 'ParallelMusicElement (send 'get-type (car lst))))
-              (cons result-lst (cons (list-builder (send 'get-elements (car lst)) result-lst) (list-builder (cdr lst) result-lst))))
+           ((and (procedure? (car x)) (eqv? 'ParallelMusicElement (send 'get-type (car x))))
+              (cons (list-builder (send 'get-elements (car x))) (list-builder (cdr x))))
            
-           (else (car lst)))
+           (else list-builder (car x)))
      
-     (cond ((and (procedure? lst) (eqv? 'Note (send 'get-type (car lst))))
-              (cons result-lst (cons (car lst) (list-builder (cdr lst) result-lst))))
+     (cond ((and (procedure? x) (eqv? 'Note (send 'get-type x)))
+              x)
            
-           ((and (procedure? lst) (eqv? 'Pause (send 'get-type (car lst))))
-              (cons result-lst (cons (car lst) (list-builder (cdr lst) result-lst))))
+           ((and (procedure? x) (eqv? 'Pause (send 'get-type x)))
+              x)
            
-           ((and (procedure? lst) (eqv? 'SequentialMusicElement (send 'get-type (car lst))))
-              (cons result-lst (cons (list-builder (send 'get-elements (car lst)) result-lst) (list-builder (cdr lst) result-lst))))
+           ((and (procedure? x) (eqv? 'SequentialMusicElement (send 'get-type x)))
+              (list-builder (send 'get-elements x)))
            
-           ((and (procedure? lst) (eqv? 'ParallelMusicElement (send 'get-type (car lst))))
-              (cons result-lst (cons (list-builder (send 'get-elements (car lst)) result-lst) (list-builder (cdr lst) result-lst))))
+           ((and (procedure? x) (eqv? 'ParallelMusicElement (send 'get-type x)))
+              (list-builder (send 'get-elements x)))
            
            (else error "Invalid musicElement")))))
 
@@ -122,21 +143,43 @@
         (cons (car lst)(remove-empty-helper (cdr lst) result-lst)))
       lst))
 
+(define (calc-duration-seq x)
+  (calc-duration-seq-helper x 0))
+
+(define (calc-duration-seq-helper x res)
+  (if(null? x)
+     res
+     (calc-duration-seq-helper (cdr x) (+ (send 'get-duration (car x)) res))))
+
+(define (calc-duration-par x)
+  (calc-duration-par-helper x 0))
+
+(define (calc-duration-par-helper x longest-duration)  ;Dis sit does not works
+  (if(null? x)
+     longest-duration
+     (if(and (not (pair? x))(procedure? x))
+        (if (> (send 'get-duration x) longest-duration)
+            (send 'get-duration x)
+            longest-duration)
+        (if(> (send 'get-duration (car x)) longest-duration)
+         (calc-duration-par-helper (cdr x) (car x))
+         (calc-duration-par-helper (cdr x) (car longest-duration))))))
+
 
 ;Constructor functions
-(define (note pitch duration instrument)
+(define (note pitch duration instrument start)
   (if(and (pitch? pitch) (duration? duration) (instrument? instrument))
      (let ((pitch-value pitch)
         (duration-value duration)
         (instrument-type instrument)
-        (start-time starttime))    
+        (start-time start))    
 
        (define (get-pitch) pitch-value)
        (define (get-duration) duration-value)
        (define (get-instrument) instrument-type)
        (define (get-start-time) start-time)
-       (define (scale factor) (note pitch-value (* factor duration-value) instrument-type start-time))
-       (define (transpose amount) (note (+ amount pitch-value) duration-value instrument-type start-time))
+       (define (scale factor) (note pitch-value (* factor duration-value) instrument-type))
+       (define (transpose amount) (note (+ amount pitch-value) duration-value instrument-type))
        (define (re-instrument new-instrument) (note pitch-value duration-value new-instrument))
        (define (get-type) 'Note)
        (define (self message)
@@ -151,10 +194,10 @@
        self)
      (error "Invalid note!")))
     
-(define (pause duration)
+(define (pause duration start)
   (if(duration? duration)
      (let ((duration-value duration)
-           (start-time starttime))
+           (start-time start))
 
        (define (get-duration) duration-value)
        (define (get-type) 'Pause)
@@ -172,8 +215,10 @@
 
        (define (get-elements) elements-list)
        (define (get-type) 'SequentialMusicElement)
+       (define (get-duration) (calc-duration-seq elements-list))
        (define (self message)
          (cond ((eqv? message 'get-elements) get-elements)
+               ((eqv? message 'get-duration) get-duration)
                ((eqv? message 'get-type) get-type)))
        self)
      (error "Invalid SequentialMusicElement")))
@@ -184,8 +229,10 @@
 
        (define (get-elements) elements-list)
        (define (get-type) 'ParallelMusicElement)
+       (define (get-duration) (calc-duration-par elements-list))
        (define (self message)
          (cond ((eqv? message 'get-elements) get-elements)
+               ((eqv? message 'get-duration) get-duration)
                ((eqv? message 'get-type) get-type)))
        self)
      (error "Invalid ParallelMusicElement")))
@@ -194,7 +241,9 @@
 
 
 ;For testing
-(define tnote (note 1 2000 3))
-(define tpause (pause 22000))
-(define tSME (SequentialMusicElement '(tnote tpause)))
-(define tPME (ParallelMusicElement '(tnote tpause tSME)))
+(define tnote (note 1 2000 3 0))
+(define tpause (pause 22000 3000))
+(define tSME (SequentialMusicElement tnote tpause))
+(define tPME (ParallelMusicElement tnote tpause tSME))
+(define test (SequentialMusicElement tSME tnote tPME tpause))
+(define a (list-builder test))
