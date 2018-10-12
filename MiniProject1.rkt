7f01304651735e86a0a9d7f2de40fd0730e2ae8d
Saftevand <m010795@hotmail.com>
2018-10-12 21:40:59 +0200

    Oprydning og kommentarer

--- MiniProject1.rkt
+++ MiniProject1.rkt
@@ -6,7 +6,16 @@
 
 (require "music-base.rkt")
 
-(define velocity 80) ;keeping the velocity constant
+;---------------------------------------------;
+;                                             ;
+;      ---BEFORE RUNNING THE PROGRAM---       ;
+;  The program will automatically generate    ;
+;  a midi file of the example song.           ;
+;  If this is not wanted, then simply         ;
+;  commen out the last line of this document  ;
+;                                             ;
+;---------------------------------------------;
+
 
 ;Predicates
 (define (pitch? pitchvalue)
@@ -61,18 +70,8 @@
         (can-create-sequential-or-parallel-MusicElement-helper (cdr x))
         #f)))
 
-
-
-
-(define (convert-to-note-abs-time-with-duration seq-mus-elem)
-  (remove-empty(list-builder seq-mus-elem '())))
-
-(define (convert-to-note-abs-time-with-duration-note x)
-  (note-abs-time-with-duration (send 'get-start-time x) (send 'get-instrument x) (send 'get-pitch x) velocity (send 'get-duration x)))
-
-(define (convert-to-note-abs-time-with-duration-pause x)
-  (note-abs-time-with-duration (send 'get-start-time x) 1 1 0 (send 'get-duration x)))
-
+;Given a music element, it will create note-abs-time-with-duration, by converting
+;the music element(and its sup-elements) into the corect format of note-abs-time-with-duration
 (define (convert-to-note-abs-time-with-duration-recursive x)
   (if(null? x)
      '()
@@ -103,48 +102,33 @@
            (else error "Error: Unkown type"))
          (append (convert-to-note-abs-time-with-duration-recursive (car x)) (convert-to-note-abs-time-with-duration-recursive (cdr x)))))))
 
+;Converts a note to note-abs-time-with-duration 
+(define (convert-to-note-abs-time-with-duration-note x)
+  (note-abs-time-with-duration (send 'get-start-time x) (send 'get-instrument x) (send 'get-pitch x) velocity (send 'get-duration x)))
 
-(define (list-builder x)
-  (if(null? x)
-     '()
-     (if (pair? x)
-     (cond ((and (procedure? (car x)) (eqv? 'Note (send 'get-type (car x))))
-              (cons (car x) (list-builder (cdr x))))           
-           ((and (procedure? (car x)) (eqv? 'Pause (send 'get-type (car x))))
-              (cons (car x) (list-builder (cdr x))))           
-           ((and (procedure? (car x)) (eqv? 'SequentialMusicElement (send 'get-type (car x))))
-              (cons (list-builder (send 'get-elements (car x))) (list-builder (cdr x))))           
-           ((and (procedure? (car x)) (eqv? 'ParallelMusicElement (send 'get-type (car x))))
-              (cons (list-builder (send 'get-elements (car x))) (list-builder (cdr x))))           
-           (else list-builder (car x)))
-     
-     (cond ((and (procedure? x) (eqv? 'Note (send 'get-type x))) x)           
-           ((and (procedure? x) (eqv? 'Pause (send 'get-type x))) x)           
-           ((and (procedure? x) (eqv? 'SequentialMusicElement (send 'get-type x))) (list-builder (send 'get-elements x)))           
-           ((and (procedure? x) (eqv? 'ParallelMusicElement (send 'get-type x))) (list-builder (send 'get-elements x)))           
-           (else error "Invalid musicElement")))))
+;Converts a pause to a note-abs-time-with-duration
+(define (convert-to-note-abs-time-with-duration-pause x)
+  (note-abs-time-with-duration (send 'get-start-time x) 1 1 0 (send 'get-duration x)))
 
-(define (remove-empty lst)
-  (remove-empty-helper lst '()))
-
-(define (remove-empty-helper lst result-lst)
-  (if (pair? lst)
-      (if(null? (car lst))
-        (remove-empty-helper (cdr lst) result-lst)
-        (cons (car lst)(remove-empty-helper (cdr lst) result-lst)))
-      lst))
-
+;Given a sequential music element, it will find the sum of its elements
+;duration, which will be the duration of the sequential music element
 (define (calc-duration-seq x)
   (calc-duration-seq-helper x 0))
 
+;Helper function for calc-duration-seq, which finds the total sum of all the
+;durations of the elements in a sequential music element
 (define (calc-duration-seq-helper x res)
   (if(null? x)
      res
      (calc-duration-seq-helper (cdr x) (+ (send 'get-duration (car x)) res))))
 
+;Given a parallel music element, it will return the duration of the element
+;with the longest duration, which is the same as the parallel music elements duration
 (define (calc-duration-par x)
   (calc-duration-par-helper x 0))
 
+;Helper function for calc-duration-par, which finds the element with the longest duration
+;in a parallel music element, throught the use of longest-duration
 (define (calc-duration-par-helper x longest-duration)
   (if(null? x)
      longest-duration
@@ -156,6 +140,8 @@
            (calc-duration-par-helper (cdr x) (send 'get-duration (car x)))
            (calc-duration-par-helper (cdr x) longest-duration)))))
 
+;Given an parallel music element, it will set the start time for the element
+;as well as the elements it contains, so that all its elements will start at the same time
 (define (set-start-time-par x time)
   (if(null? x)
      '()
@@ -176,6 +162,8 @@
                 (cons (set-start-time-par (car x) time) (set-start-time-par (cdr x) time)))
              (else error "Error: Unkown type")))))
 
+;Given an sequential music element, it will set the start time for the element
+;as well as the elements it contains, so that the elements will start one after the other
 (define (set-start-time-seq x time)
   (if(null? x)
      '()
@@ -216,17 +204,8 @@
              (else error "Error: Unkown type"))
            (transpose-element (car x) amount)))))
 
-(define (convert-to-pitch x) ; Ved ikke om den her skal bruges?
-  (cond((eqv? x ) "Piano")
-       ((eqv? x 2) "Organ")
-       ((eqv? x 3) "Guitar")
-       ((eqv? x 4) "Violin")
-       ((eqv? x 5) "Flute")
-       ((eqv? x 6) "Trumpet")
-       ((eqv? x 7) "Helicopter")
-       ((eqv? x 8) "Telephone")
-       (else (error "Invalid instrument"))))
 
+;Given the name of an instrument, it will return the instruments id
 (define (convert-from-instrument x) ; "" -> '
   (cond((eqv? x 'Piano) 1)
        ((eqv? x 'Organ) 2)
@@ -236,11 +215,11 @@
        ((eqv? x 'Trumpet) 6)
        ((eqv? x 'Helicopter) 7)
        ((eqv? x 'Telephone)  8)
-       (else (error "Invalid instrument"))))
+       (else (error "Unknown instrument"))))
         
 
 
-;Constructor functions
+;The music elements "created" as ojects
 (define (note pitch duration instrument start)
   (if(and (pitch? pitch) (duration? duration) (instrument? instrument))
      (let ((pitch-value pitch)
@@ -315,19 +294,16 @@
        self)
      (error "Invalid ParallelMusicElement")))
 
+;Used to the duriation of a note, so that it is easier to create notes from a piano sheet
+;However it should only be used for songs with the same BPM as the one created below
+(define (note-duration x)
+  (* x 450))
 
+;keeping the velocity constant
+(define velocity 80) 
 
 
 ;The song
-(define (note-duration x)
-  (* x 420))
-;960 midi = 1 sec
-;BPM 180
-
-;(note 5 (note-duration 1) (convert-from-instrument ') 0)
-;(pause (note-duration 5) 0)
-
-
 (define TrebleClef1 (SequentialMusicElement (pause (note-duration 1) 0)
                                            (note 59 (note-duration 1) (convert-from-instrument 'Piano) 0)
                                            (note 57 (note-duration 1) (convert-from-instrument 'Piano) 0) ;1
@@ -353,27 +329,37 @@
                                            (note 59 (note-duration 1) (convert-from-instrument 'Piano) 0)
                                            (note 55 (note-duration 4) (convert-from-instrument 'Piano) 0)))
 
-(define BassClef-B(SequentialMusicElement;d
+(define BassClef-D(SequentialMusicElement
   (note 50 (note-duration 3) (convert-from-instrument 'Piano) 0)
   (note 50 (note-duration 3) (convert-from-instrument 'Piano) 0)
   (note 50 (note-duration 2) (convert-from-instrument 'Piano) 0)))
-(define BassClef-A(SequentialMusicElement;c
+(define BassClef-C(SequentialMusicElement
   (note 48 (note-duration 3) (convert-from-instrument 'Piano) 0)
   (note 48 (note-duration 3) (convert-from-instrument 'Piano) 0) 
   (note 48 (note-duration 2) (convert-from-instrument 'Piano) 0)))
-(define BassClef-G(SequentialMusicElement;b
+(define BassClef-B(SequentialMusicElement
   (note 47 (note-duration 3) (convert-from-instrument 'Piano) 0)
   (note 47 (note-duration 3) (convert-from-instrument 'Piano) 0)
   (note 47 (note-duration 2) (convert-from-instrument 'Piano) 0)))
-(define BassClef-C(SequentialMusicElement;e
+(define BassClef-E(SequentialMusicElement
   (note 52 (note-duration 3) (convert-from-instrument 'Piano) 0)
   (note 52 (note-duration 3) (convert-from-instrument 'Piano) 0)
   (note 52 (note-duration 2) (convert-from-instrument 'Piano) 0)))
 
 (define BassClef1 (SequentialMusicElement (pause (note-duration 4) 0) ;1
-                                          BassClef-A                  ;2
-                                          BassClef-B                  ;3
-                                          BassClef-G))                ;4
+                                          BassClef-C                  ;2
+                                          BassClef-D                  ;3
+                                          BassClef-B))                ;4
+
+(define BassClef2 (SequentialMusicElement BassClef1
+                                          BassClef-E                  ;5
+                                          BassClef-C                  ;6
+                                          BassClef-D                  ;7
+                                          BassClef-B                  ;8
+                                          BassClef-E                  ;9
+                                          BassClef-C                  ;10
+                                          BassClef-D                  ;11
+                                          BassClef-B))                ;12
 
 (define TrebleClef2 (SequentialMusicElement TrebleClef1
                                             (note 59 (note-duration 1) (convert-from-instrument 'Piano) 0)
@@ -432,34 +418,11 @@
                                             (note 55 (note-duration 3) (convert-from-instrument 'Piano) 0)
                                             ))
 
-(define BassClef2 (SequentialMusicElement BassClef1
-                                          BassClef-C   ;5
-                                          BassClef-A   ;6
-                                          BassClef-B   ;7
-                                          BassClef-G   ;8
-                                          BassClef-C   ;9
-                                          BassClef-A   ;10
-                                          BassClef-B   ;11
-                                          BassClef-G)) ;12
-
-
 (define lovescenario (SequentialMusicElement (ParallelMusicElement TrebleClef1 BassClef1) (ParallelMusicElement TrebleClef2 BassClef2)))
-
-
-
-;For testing
-;(define tnote (note 90 15 (convert-from-instrument 'Flute) 0))
-;(define tnote2 (note 60 (note-duration 1) (convert-from-instrument 'Piano) 0))
-;(define tpause (pause 500 0))
-;(define tSME (SequentialMusicElement tnote2 tpause))
-;(define tPME (ParallelMusicElement tnote2 tnote2 tSME))
-
-
-;(define longtest1 (SequentialMusicElement tnote2 tpause tnote2 tpause))
-;(define longtest (SequentialMusicElement longtest1 longtest1))
-
-;(define li (SequentialMusicElement longtest longtest longtest));(transpose-element longtest 10) (transpose-element longtest 20)))
-(define lt (set-start-time-seq lovescenario 1000))
-
-(define song (convert-to-note-abs-time-with-duration-recursive lt))
-(transform-to-midi-file-and-write-to-file! song "test.midi")
+;I shifted the start of the song with ~a second to give the audio player a change to load
+;as i experienced windows media player to sometimes skip the first note or two as it
+;was loading the song
+(define lovescenario-with-correct-start (set-start-time-seq lovescenario 1000))
+(define song (convert-to-note-abs-time-with-duration-recursive lovescenario-with-correct-start))
+;If it is not wanted to have the program output a media file, then comment out the following line
+(transform-to-midi-file-and-write-to-file! song "Mlha15TestSong.midi")
